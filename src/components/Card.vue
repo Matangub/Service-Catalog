c1238a079e7f7ef4dad2a7b8149613aead1e5185
Matan Gubkin <Matangub@gmail.com>
2021-11-06 23:31:28 +0200

    separating markup to components, improved style and logic

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,16 +1,38 @@
-<script setup lang="ts">
+<script lang="ts">
+import { ref, PropType } from 'vue'
 const images = {
     nodejs: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg",
     python: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
 }
-const img = images['python']
+
+interface Props {
+    img: keyof typeof images
+}
+
+export default {
+    props: {
+        img: {
+            required: true,
+            type: String as PropType<keyof typeof images>,
+        }
+    },
+    setup(props: Props) {
+        const imgLogo = ref('imgLogo')
+        imgLogo.value = images[props.img]
+        return {
+            imgLogo: imgLogo.value
+        }
+    }
+}
 </script>
 
 <template>
-    <div class="w-full h-48 rounded bg-green-200 m-5 py-4 px-4 shadow-xl relative">
-        <div class="border-b border-gray-600 flex justify-between items-center align-middle">
+    <div
+        class="w-full h-52 rounded bg-gray-200 border-gray-300 border m-5 py-2 px-4 shadow-xl relative"
+    >
+        <div class="border-b border-gray-600 pb-1 flex justify-between items-center align-middle">
             <h4 class="text-left text-xl flex gap-1 w-full">
-                <img class="w-8" :src="img" />
+                <img class="w-8" :src="imgLogo" />
                 <span class="flex items-end">Frontend</span>
             </h4>
             <svg
@@ -30,21 +52,29 @@ const img = images['python']
         </div>
         <ul class="py-4 flex flex-col text-sm">
             <li class="flex">
-                <span class="mr-5 font-medium min-w-max w-24">Created At:</span>
+                <span class="mr-5 font-medium min-w-max w-20">Name:</span>
+                <span>Cart Service</span>
+            </li>
+            <li class="flex">
+                <span class="mr-5 font-medium min-w-max w-20">Created At:</span>
                 <span>24/6/21 2:53 am</span>
             </li>
             <li class="flex">
-                <span class="mr-5 font-medium min-w-max w-24">Owner:</span>
+                <span class="mr-5 font-medium min-w-max w-20">Owner:</span>
                 <span>John Doe</span>
             </li>
             <li class="flex">
-                <span class="mr-5 font-medium min-w-max w-24">Stage:</span>
+                <span class="mr-5 font-medium min-w-max w-20">Stage:</span>
                 <span>CI</span>
             </li>
+            <li class="flex">
+                <span class="mr-5 font-medium min-w-max w-20">Framework:</span>
+                <span>Node</span>
+            </li>
         </ul>
-        <div class="text-right w-full">
+        <div class="text-right w-full absolute bottom-2 right-2">
             <button
-                class="rounded-md text-white inset-y-full bg-green-800 border-gray-500 p-2"
+                class="rounded-2xl text-white inset-y-full bg-indigo-800 border-gray-500 p-2"
             >Action</button>
         </div>
     </div>
---
+++ src/components/DropdownSelect.vue
@@ -0,0 +1,60 @@
+<script lang="ts">
+export default {
+    name: 'DropdownSelect',
+    setup() {
+
+    }
+}
+</script>
+
+<template>
+    <div class="dropdown inline-block relative w-full">
+        <button
+            class="bg-white border-gray-300 border text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center w-full flex justify-between"
+        >
+            <span class="mr-1">Dropdown</span>
+            <svg
+                class="fill-current h-4 w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+            >
+                <path
+                    d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                />
+            </svg>
+        </button>
+        <div
+            class="dropdown-menu p-2 absolute hidden text-gray-700 border border-gray-300 bg-white shadow-lg pt-1 z-10 w-full"
+        >
+            <label class="inline-flex items-center">
+                <input type="checkbox" class="form-checkbox text-indigo-600" checked />
+                <span class="ml-2">Select All</span>
+            </label>
+            <div class="mx-4 my-2 flex-col flex">
+                <label class="inline-flex items-center">
+                    <input type="checkbox" class="form-checkbox" checked />
+                    <span class="ml-2">Nodejs</span>
+                </label>
+                <label class="inline-flex items-center">
+                    <input type="checkbox" class="form-checkbox" checked />
+                    <span class="ml-2">Python</span>
+                </label>
+                <label class="inline-flex items-center">
+                    <input type="checkbox" class="form-checkbox" checked />
+                    <span class="ml-2">Go</span>
+                </label>
+            </div>
+            <div class="flex flex-row-reverse w-full border-t border-gray-300 pt-2">
+                <button
+                    class="rounded-3xl text-white inset-y-full bg-indigo-800 border-gray-500 py-2 px-6"
+                >Filter</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.dropdown:hover .dropdown-menu {
+    display: block;
+}
+</style>
